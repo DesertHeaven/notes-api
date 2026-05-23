dd0ee5859067ee45eaefdf2736e7567146a1c99a
DesertHeaven <dev.shm7@gmail.com>
2026-05-23 15:09:50 +0300

    feat: add get note by id endpoint

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ app.get("/notes", (req, res) => {
   res.json(notes);
 });
 
+app.get("/notes/:id", (req, res) => {
+  const noteId = Number(req.params.id);
+
+  const note = notes.find((note) => note.id === noteId);
+
+  if (!note) {
+    return res.status(404).json({
+      message: "Note not found",
+    });
+  }
+
+  res.json(note);
+});
+
 app.post("/notes", (req, res) => {
   const newNote = req.body;
 
